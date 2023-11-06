a7909d329b24caa67118fe22a318408ccaee711e
Phoenix <awesomepcat3@gmail.com>
2023-11-06 16:24:19 -0500

    Update categories.ts

--- src/data/categories.ts
+++ src/data/categories.ts
@@ -1,6 +1,8 @@
 // List of categories for blog posts
 export const CATEGORIES = [
-	'News',
-	'redcatOS',
-	'Lightning'
+	'Category 1',
+	'Category 2',
+	'Category 3',
+	'Category 4',
+	'Category 5'
 ] as const
